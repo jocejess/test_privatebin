e6f52b81f4b69d166f43c475bf49337967aee332
jocejess <146317403+jocejess@users.noreply.github.com>
2024-01-22 15:21:34 +0100

    Update Bootstrap.php

--- tst/Bootstrap.php
+++ tst/Bootstrap.php
@@ -24,7 +24,7 @@ if (!defined('CONF_SAMPLE')) {
     define('CONF_SAMPLE', PATH . 'cfg' . DIRECTORY_SEPARATOR . 'conf.sample.php');
 }
 
-require PATH . 'vendor/autoload.php';
+//require PATH . 'vendor/autoload.php';
 Helper::updateSubresourceIntegrity();
 
 /**
